7245a1f3cfca4d8590017868fd5ce150652997ad
Carolyn Cole <cam156@psu.edu>
2012-10-08 14:12:22 -0400

    Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydracamp::Application.config.secret_token = 'a61191ca4f3456a29797e800d4d82620a7f495503c72d1d47b62a9ca6a19bf339bd1cb9df795a410114e3fb957e9376bd9ab75151dbc21d131bb97fbb11ad608'
